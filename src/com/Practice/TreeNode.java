72d6802a1f1c77dc89f8c48f670cb3446d5e234c
Rajat Porwal <rajatit3057@gmail.com>
2021-11-06 21:27:05 +0530

    Overloaded the constructor !

--- src/com/Practice/TreeNode.java
+++ src/com/Practice/TreeNode.java
@@ -10,4 +10,10 @@ public class TreeNode {
 		this.leftChild=null;
 		this.rightChild=null;
 	}
+	
+	TreeNode(String val, TreeNode left, TreeNode right){
+		this.value=val;
+		this.leftChild=left;
+		this.rightChild=right;
+	}
 }
